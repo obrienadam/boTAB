d67efd36ed39ea2482926698ba7f7c5ce2a52f8b
Adam O'Brien <roni511@gmail.com>
2014-11-09 23:25:47 -0500

    The model is running, but their appear to be some bugs. This is not a working version.

--- boTAB.py
+++ boTAB.py
@@ -1,4 +1,4 @@
-#!/bin/usr/env python2
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
 """
@@ -6,7 +6,7 @@
 boTAB
 =====
 
-This solver uses the popular TAB model to simulate the atomization of sprays.
+This solver uses the popular TAB model to simulate the atomization of droplets
 
 Author: Adam O'Brien
 
@@ -15,14 +15,30 @@ Author: Adam O'Brien
 from math import exp, cos, sin
 from fluid import Freestream, Droplet, Vector
 
+# Returns the axis distortion y^n+1
+
 def computeY(Wec, dt, td, t, omega, yn, dydtn):
+    
     return Wec + exp(-dt/td)*((yn - Wec)*cos(omega*t) + 1/omega*(dydtn + \
             (yn - Wec)/td)*sin(omega*t))
             
-def computdYdt(Wec, dt, td, t, omega, yn, dydtn):
-    pass
+# Returns the time rate of change of axis distortion (dy/dt)^n+1     
+            
+def computeDyDt(Wec, dt, td, t, omega, yn1, yn, dydtn):
+    
+    return (Wec - yn1)/td + omega*exp(-dt/td)*(1/omega*(dydtn + (yn - Wec)/td)* \
+            cos(omega*dt) - (yn - Wec)*sin(omega*dt))
 
 def main():
+    
+    # Define the empirical constants
+    
+    Cb = 0.5
+    Ck = 8.
+    Cd = 5.
+    Cf = 1/3.
+    K = 10/3.
+    Cv = 1.
     
     freestream = Freestream(1.205,             # density
                             18.27e-6,          # viscosity
@@ -34,6 +50,74 @@ def main():
                       0.07262,          # surface tension coefficient
                       Vector(0., 0.),   # position
                       Vector(0., 0.))   # velocity
+                      
+    # Define simulation parameters
+                      
+    maxTime = 0.1
+    nTimeSteps = 1000
+    dt = maxTime/nTimeSteps
+    nBreakups = 0
+    
+    # Begin the simulation
+    
+    for stepNo in range(1, nTimeSteps + 1):
+        
+        # Compute relevant droplet oscillation parameters
+    
+        We = droplet.weberNo(freestream)
+        Wec = droplet.weberNoCrit(freestream, Cf, Ck, Cb)       
+        td = droplet.td(Cd)
+        omega = droplet.omega(Cd, Ck)
+        
+        # Compute the droplet distortion       
+        
+        yn = droplet.y
+        droplet.y = computeY(Wec, dt, td, droplet.t, omega, droplet.y, \
+        droplet.dydt)
+        droplet.dydt = computeDyDt(Wec, dt, td, droplet.t, omega, droplet.dydt, \
+        yn, droplet.dydt)
+        
+        #print droplet.y
+        
+        # Advect the droplet
+        
+        
+        
+        
+        
+        droplet.advect(freestream, Cb, dt)
+        
+        # Check for breakup. Create a new droplet in the event of breakup
+        
+        if droplet.checkBreakup():
+            
+            # Compute the Sauter Mean Radius (SMR) of the new droplet
+            # distribution. For now, only one droplet is tracked            
+            """
+            r32 = droplet.radius/(1. + 8.*K*droplet.y**2/20. + \
+            droplet.rho*droplet.radius**3*droplet.dydt**2/droplet.sigma* \
+            (6.*K - 5.)/120.)
+            """
+            r32 = droplet.radius/2
+            # Compute the velocity of the new child droplet. Assume it just
+            # travels normal to the path of the parent droplet
+
+            v = droplet.velocity
+
+
+            # Create new droplet
+            
+            droplet = Droplet(r32,   # radius
+                      998.,             # density
+                      8.94e-4,          # viscosity
+                      0.07262,          # surface tension coefficient
+                      droplet.position,   # position
+                      v)   # velocity
+                      
+            nBreakups += 1
+                      
+            print droplet
+        
     
 # Execute the main function    
     
--- fluid.py
+++ fluid.py
@@ -13,28 +13,53 @@ Author: Adam O'Brien
 
 """
 
+from math import sqrt, pi
+
 # Vector class for position/velocity
 
 class Vector(object):
+    
     def __init__(self, x, y):
         self.x = x
         self.y = y
         
+    def __repr__(self):
+        
+        return "(%s, %s)"%(self.x, self.y)
+        
     def __add__(self, other):
+        
         return Vector(self.x + other.x, self.y + self.y)
             
     def __sub__(self, other):
+        
         return Vector(self.x - other.x, self.y - other.y)
         
+    def mag(self):
+        return sqrt(self.x**2 + self.y**2)
+        
+    def normalVector(self):
+        return Vector(self.y, -self.x)
+        
+    def scale(self, other):
+        return Vector(self.x*other, self.y*other)
+        
     def rVector(self, other):
+        
         return other - self
+        
+    def unitVector(self):
+        
+        return self.scale(1./self.mag())
             
 def dot(u, v):
+    
     return u.x*v.x + u.y*v.y
     
 # Freestream class for representing the freestream flow
 
 class Freestream(object):
+    
     def __init__(self, rho, mu, velocity):
         self.rho = rho
         self.mu = mu
@@ -43,6 +68,7 @@ class Freestream(object):
 # Droplet class for representing droplets    
     
 class Droplet(object):
+    
     def __init__(self, radius, rho, mu, sigma, position, velocity):
         self.radius = radius
         self.rho = rho
@@ -50,12 +76,52 @@ class Droplet(object):
         self.sigma = sigma
         self.position = position
         self.velocity = velocity
+        self.mass = 4.*pi*self.rho*self.radius**3/3
+        self.area = pi*self.radius**2
+        self.y = 0.
+        self.dydt = 0.
+        self.t = 0.
+        
+    def __repr__(self):
+        
+        return "Radius: %s\nPosition: %s\nVelocity: %s"%(str(self.radius), \
+        self.position, self.velocity)
         
     def weberNo(self, freestream):
+        
         vRel = self.velocity.rVector(freestream.velocity)
         
         return freestream.rho*dot(vRel, vRel)*self.radius/self.sigma
         
     def weberNoCrit(self, freestream, Cf, Ck, Cb):
+        
         return Cf/(Ck*Cb)*self.weberNo(freestream)
         
+    def td(self, Cd):
+        
+        return 2.*self.rho*self.radius ** 2/(Cd*self.mu)
+        
+    def omega(self, Cd, Ck):
+        
+        td = self.td(Cd)
+        
+        return sqrt(Ck*self.sigma/(self.rho*self.radius ** 3) - 1./td**2)
+        
+    def advect(self, freestream, Cb, dt):
+        
+        vRel = self.velocity.rVector(freestream.velocity)        
+        
+        Fd = vRel.unitVector().scale(Cb*freestream.rho*dot(vRel, vRel)/2*self.area)
+        
+        a = Fd.scale(1./self.mass)
+        
+        self.position += self.velocity.scale(dt) + a.scale(dt**2/2)
+        
+        self.velocity += a.scale(dt)
+        
+    def checkBreakup(self):
+        
+        if self.y > 1.:
+            return True
+        else:
+            return False
